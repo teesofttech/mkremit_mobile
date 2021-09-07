41144ccd6c9ab14ee9b90f40dcdee770c2d4a7e2
Esanju Babatunde <tundeesanju@gmail.com>
2021-09-07 13:06:43 +0100

    commit

--- app/src/main/java/com/teesofttech/mkremit/splashscreen/SplashscreenActivity.java
+++ app/src/main/java/com/teesofttech/mkremit/splashscreen/SplashscreenActivity.java
@@ -1,49 +1,50 @@
 package com.teesofttech.mkremit.splashscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.teesofttech.mkremit.MainActivity;
 import com.teesofttech.mkremit.R;
 import com.teesofttech.mkremit.Utils.PrefManager;
+import com.teesofttech.mkremit.Utils.SlideViewPagerAdapter;
 import com.teesofttech.mkremit.loginutils.LoginActivity;
 
 public class SplashscreenActivity extends AppCompatActivity {
-    private PrefManager prefManager;
+    public static ViewPager viewPager;
+    SlideViewPagerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
 
-        prefManager = new PrefManager(this);
-        if (!prefManager.isFirstTimeLaunch()) {
-            launchHomeScreen();
-            finish();
+        viewPager = findViewById(R.id.viewpager);
+        adapter = new SlideViewPagerAdapter(this);
+        viewPager.setAdapter(adapter);
+        if (isOpenAlread()) {
+            Intent intent = new Intent(SplashscreenActivity.this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
         } else {
-            requestWindowFeature(Window.FEATURE_NO_TITLE);
-            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
-            setContentView(R.layout.activity_splashscreen);
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    final Intent mainIntent = new Intent(SplashscreenActivity.this, LoginActivity.class);
-                    SplashscreenActivity.this.startActivity(mainIntent);
-                    SplashscreenActivity.this.finish();
-                }
-            }, 2000);
+            SharedPreferences.Editor editor = getSharedPreferences("slide", MODE_PRIVATE).edit();
+            editor.putBoolean("slide", true);
+            editor.commit();
         }
+
     }
 
-    private void launchHomeScreen() {
+    private boolean isOpenAlread() {
 
-        prefManager.setFirstTimeLaunch(false);
-        startActivity(new Intent(SplashscreenActivity.this, LoginActivity.class));
-        finish();
+        SharedPreferences sharedPreferences = getSharedPreferences("slide", MODE_PRIVATE);
+        boolean result = sharedPreferences.getBoolean("slide", false);
+        return result;
 
     }
 }
