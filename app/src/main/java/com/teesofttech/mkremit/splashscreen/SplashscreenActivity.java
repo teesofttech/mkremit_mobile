499ff8d72b5ecad9c8fcff5920d168a3904efad5
Esanju Babatunde <tundeesanju@gmail.com>
2021-09-09 09:23:33 +0100

    commit

--- app/src/main/java/com/teesofttech/mkremit/splashscreen/SplashscreenActivity.java
+++ app/src/main/java/com/teesofttech/mkremit/splashscreen/SplashscreenActivity.java
@@ -1,50 +1,154 @@
 package com.teesofttech.mkremit.splashscreen;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
+import android.text.Html;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-import com.teesofttech.mkremit.MainActivity;
 import com.teesofttech.mkremit.R;
 import com.teesofttech.mkremit.Utils.PrefManager;
-import com.teesofttech.mkremit.Utils.SlideViewPagerAdapter;
 import com.teesofttech.mkremit.loginutils.LoginActivity;
 
 public class SplashscreenActivity extends AppCompatActivity {
-    public static ViewPager viewPager;
-    SlideViewPagerAdapter adapter;
+    ViewPager mSLideViewPager;
+    LinearLayout mDotLayout;
+    Button backbtn, nextbtn, skipbtn;
 
+    TextView[] dots;
+    ViewPagerAdapter viewPagerAdapter;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splashscreen);
 
-        viewPager = findViewById(R.id.viewpager);
-        adapter = new SlideViewPagerAdapter(this);
-        viewPager.setAdapter(adapter);
-        if (isOpenAlread()) {
-            Intent intent = new Intent(SplashscreenActivity.this, MainActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
-        } else {
-            SharedPreferences.Editor editor = getSharedPreferences("slide", MODE_PRIVATE).edit();
-            editor.putBoolean("slide", true);
-            editor.commit();
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splashscreen);
+        backbtn = findViewById(R.id.backbtn);
+        nextbtn = findViewById(R.id.nextbtn);
+        skipbtn = findViewById(R.id.skipButton);
+
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (getitem(0) > 0) {
+
+                    mSLideViewPager.setCurrentItem(getitem(-1), true);
+
+                }
+
+            }
+        });
+
+        nextbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (getitem(0) < 3)
+                    mSLideViewPager.setCurrentItem(getitem(1), true);
+                else {
+
+                    Intent i = new Intent(SplashscreenActivity.this, LoginActivity.class);
+                    startActivity(i);
+                    finish();
+
+                }
+
+            }
+        });
+
+        skipbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Intent i = new Intent(SplashscreenActivity.this, mainscreen.class);
+                startActivity(i);
+                finish();
+
+            }
+        });
+
+        mSLideViewPager = (ViewPager) findViewById(R.id.slideViewPager);
+        mDotLayout = (LinearLayout) findViewById(R.id.indicator_layout);
+
+        viewPagerAdapter = new ViewPagerAdapter(this);
+
+        mSLideViewPager.setAdapter(viewPagerAdapter);
+
+        setUpindicator(0);
+        mSLideViewPager.addOnPageChangeListener(viewListener);
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void setUpindicator(int position) {
+
+        dots = new TextView[4];
+        mDotLayout.removeAllViews();
+
+        for (int i = 0; i < dots.length; i++) {
+
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226"));
+            dots[i].setTextSize(35);
+            dots[i].setTextColor(getResources().getColor(R.color.inactive, getApplicationContext().getTheme()));
+            mDotLayout.addView(dots[i]);
+
         }
 
-    }
-
-    private boolean isOpenAlread() {
-
-        SharedPreferences sharedPreferences = getSharedPreferences("slide", MODE_PRIVATE);
-        boolean result = sharedPreferences.getBoolean("slide", false);
-        return result;
+        dots[position].setTextColor(getResources().getColor(R.color.active, getApplicationContext().getTheme()));
 
     }
+
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.M)
+        @Override
+        public void onPageSelected(int position) {
+
+            setUpindicator(position);
+
+            if (position > 0) {
+
+                backbtn.setVisibility(View.VISIBLE);
+
+            } else {
+
+                backbtn.setVisibility(View.INVISIBLE);
+
+            }
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+
+    private int getitem(int i) {
+
+        return mSLideViewPager.getCurrentItem() + i;
+    }
+
+
 }
---
+++ app/src/main/java/com/teesofttech/mkremit/splashscreen/mainscreen.java
@@ -0,0 +1,16 @@
+package com.teesofttech.mkremit.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.teesofttech.mkremit.R;
+
+public class mainscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainscreen);
+    }
+}
